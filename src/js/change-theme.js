15ba23b73df0781dd779b4b5a01831848c1c5674
DanyilYatskov <68867876+DanyilYatskov@users.noreply.github.com>
2021-01-20 20:47:21 +0200

    fix themes

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -3,18 +3,22 @@ const refs = {
   body: document.querySelector('body'),
   themeSwitch: document.querySelector('.theme-switch__toggle'),
 };
-refs.body.classList = localStorage.getItem('theme');
-refs.themeSwitch.checked = refs.body.classList.contains(theme.DARK)
-  ? true
-  : false;
+if (localStorage.getItem('theme')) {
+  refs.body.classList.add(localStorage.getItem('theme'));
+  refs.themeSwitch.checked = refs.body.classList.contains(theme.DARK)
+    ? true
+    : false;
+}
+
 refs.themeSwitch.addEventListener('input', changeThemeHandler);
 function changeThemeHandler() {
   if (refs.themeSwitch.checked) {
     refs.body.classList.add(theme.DARK);
     refs.body.classList.remove(theme.LIGHT);
+    localStorage.setItem('theme', theme.DARK);
   } else {
     refs.body.classList.add(theme.LIGHT);
     refs.body.classList.remove(theme.DARK);
+    localStorage.setItem('theme', theme.LIGHT);
   }
-  localStorage.setItem('theme', refs.body.classList);
 }
